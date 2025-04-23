bc43c1755c94d81269ff8f431038227f04534eb6
Silvia <sentimentalfuturist@proton.me>
2025-04-24 00:20:29 +0200

    :speech_balloon: Add new link and new button to Links page

--- _data/changelog.js
+++ _data/changelog.js
@@ -6,5 +6,9 @@ module.exports = () => {
       date: DateTime.fromISO("2025-04-23"),
       text: "<ul><li>Finally added a real changelog (this one!)</li><li>General redesign and restructuring of the site and migration to Eleventy</li></ul>",
     },
+    {
+      date: DateTime.fromISO("2025-04-24"),
+      text: '<p>Added a new link in the <a href="/links">links</a> page, and also a new button to link to this site.</p>',
+    },
   ].sort((a, b) => b.date - a.date);
 };
