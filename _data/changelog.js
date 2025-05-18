60c0ff66a19557af21882d3a12cb5f0c8516097b
Silvia <sentimentalfuturist@proton.me>
2025-05-18 14:15:28 +0200

    💬 Add new links

--- _data/changelog.js
+++ _data/changelog.js
@@ -22,5 +22,9 @@ module.exports = () => {
       date: DateTime.fromISO("2025-05-03"),
       text: '<p>Added new links to the <a href="/links">links</a> page.</p>',
     },
+        {
+      date: DateTime.fromISO("2025-05-18"),
+      text: '<p>Another small update to add new <a href="/links">links</a>.</p>',
+    },
   ].sort((a, b) => b.date - a.date);
 };
